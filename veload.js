18c62d4b3335dbff28a307af6231c54103f2f79a
spencermize <spence.bassman@gmail.com>
2019-01-21 10:56:49 -0500

    expand search parameters for faster results

--- veload.js
+++ veload.js
@@ -57,11 +57,13 @@ const Veload = {
 		var radius = .5;
 		return setInterval(function(){
 			const maxResults = 50;
-			$.getJSON(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f01b5e40d794a63ebd9b51fd4eb985ab&lat=${currLoc.lat}&lon=${currLoc.lng}&format=json&extras=url_o,url_k,url_h&radius=${radius}&per_page=${maxResults}&tags=beautiful,pretty,sunset,sunrise,architecture&sort=interestingness-desc&content_type=1&nojsoncallback=1,has_geo=true`,function(data){
+			$.getJSON(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f01b5e40d794a63ebd9b51fd4eb985ab&lat=${currLoc.lat}&lon=${currLoc.lng}&format=json&extras=url_o,url_k,url_h&radius=${radius}&per_page=${maxResults}&tags=beautiful,pretty,sunset,sunrise,architecture,landscape,building,outdoors,trail,travel&sort=interestingness-desc&content_type=1&nojsoncallback=1,has_geo=true`,function(data){
 				var url = "";
 				if(data.photos.total>0){
 					var attempts = 0;
-					console.log(attempts < (data.photos.photo.length));
+					if(data.photos.total < 20){
+						radius = radius * 2;
+					}
 					while(!url && attempts < (data.photos.photo.length)){
 						const p = data.photos.photo[Math.floor(Math.random() * (data.photos.photo.length-1))];
 						url = p.url_o ? p.url_o : p.url_k;
@@ -69,8 +71,8 @@ const Veload = {
 						attempts++;
 					}
 					if(!url){
-						url = data.photos.photo[0].url_h;
 						radius = radius * 2;
+						url = data.photos.photo[0].url_h;
 					}					
 					if(url && `url("${url}")` != $('.bg.blurrer').css('background-image')){
 						$('<img/>').attr('src', url).on('load', function() {
