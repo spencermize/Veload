86fe6d931b5df36e87b5ff646df3fe6e64b75619
spencermize <spence.bassman@gmail.com>
2019-01-31 23:23:32 -0500

    gotta get these variables in

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //get local variables
 const fs = require('fs');
-var config = JSON.parse(fs.readFileSync(__dirname + '/config/config.json', {encoding: 'utf-8'}));
+const configExists = fs.existsSync('config/config.json');
+var config = "";
+if(configExists){
+  config = JSON.parse(fs.readFileSync(__dirname + '/config/config.json', {encoding: 'utf-8'}));
+}
 
 if(config.env == 'development'){
   'use strict';
@@ -18,9 +22,31 @@ if(config.env == 'development'){
   , app: require('./server.js')
   }).listen(config.devOps.http,config.devOps.https);
 }else{
+  // On Google Cloud Platform authentication is handled for us
+  const gcs = require('@google-cloud/storage')()
 
-  var app = require('./server.js')
-  app.listen(config.productionOps.port, () => console.log(`Veload started on port ${config.productionOps.port}!`))
+  const bucketName = `envvars.veload.bike`
+  console.log(`Downloading config from bucket envars`)
+  gcs
+    .bucket(bucketName)
+    .file('config.json')
+    .download({ destination: 'config/config.json' })
+    .then(() => {
+      gcs
+      .bucket(bucketName)
+      .file('strava_config')
+      .download({ destination: 'config/strava_config' })
+      .then(() => {
+        console.info('config downloaded successfully')
+        config = JSON.parse(fs.readFileSync(__dirname + '/config/config.json', {encoding: 'utf-8'}));
+        var app = require('./server.js')
+        app.listen(config.productionOps.port, () => console.log(`Veload started on port ${config.productionOps.port}!`))  
+      })    
+    })
+    .catch(e => {
+      console.error(`config download: There was an error: ${JSON.stringify(e, undefined, 2)}`)
+    })
+
 }
 
 
