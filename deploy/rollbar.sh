96015e6ccb7bb767194ce41876b9b11806a6bb40
spencermize <spence.bassman@gmail.com>
2019-02-07 08:29:40 -0500

    only deploy from master

--- deploy/rollbar.sh
+++ deploy/rollbar.sh
@@ -1,9 +1,11 @@
 ACCESS_TOKEN=$1
 ENVIRONMENT=production
-LOCAL_USERNAME=`whoami`
 REVISION=`git rev-parse --verify HEAD`
-curl https://api.rollbar.com/api/1/deploy/ \ 
-  -F access_token=$ACCESS_TOKEN \ 
-  -F environment=$ENVIRONMENT \ 
-  -F revision=$REVISION \ 
-  -F local_username=$LOCAL_USERNAME
+req = '{"access_token":'
+req += $ACCESS_TOKEN
+req += ',"environment":"production","revision":"'
+req += $REVISION
+req += '"}'
+curl --request POST \ 
+  --url https://api.rollbar.com/api/1/deploy/ \
+  --data $req
