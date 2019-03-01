1e589182e439905328c91f3191df371819453333
spencermize <spence.bassman@gmail.com>
2019-02-28 21:23:16 -0500

    enable phantomjs and adapt test

--- test/utils.trail.spec.js
+++ test/utils.trail.spec.js
@@ -1,31 +1,29 @@
-'use strict';
-import { should } from 'chai';
+var should = require('chai').should()
 import * as Trail from 'Utils.Trail.js';
 import V from './V.js';
 
 window.V = V;
-should = should();
 
 describe('getAvg - gets the average speed so far', function() {
-  it(`empty`, function() {
+  it('empty', function() {
       Trail.getAvg().should.be.a('number');
   });
-  it(`miles`, function() {
+  it('miles', function() {
     Trail.getAvg("miles").should.be.a('number');
   });
-  it(`meters`, function() {
+  it('meters', function() {
     Trail.getAvg("meters").should.be.a('number');
   });  
 });
 
 describe('getDistance - gets the distance so far',function(){
-  it(`empty`, function() {
+  it('empty', function() {
     Trail.getDistance().should.be.a('number');
   });  
-  it(`meters / estimate`, function() {
+  it('meters / estimate', function() {
     Trail.getDistance("meters").should.be.a('number');
   });
-  it(`meters / verified only`, function() {
+  it('meters / verified only', function() {
     Trail.getDistance("meters",true).should.be.a('number');
   });  
 })
